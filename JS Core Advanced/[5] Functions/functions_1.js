ca91bbeb76c8e3d7b4e56512758e55e66f75d707
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2024-04-10 16:27:05 +0300

    fix: functions task

--- JS Core Advanced/[5] Functions/functions_1.js
+++ JS Core Advanced/[5] Functions/functions_1.js
@@ -1,8 +1,17 @@
 /*Напишите функцию, которая многократно выполняет заданную операцию через фиксированный интервал, 
 используя setInterval(). */
 
-const doSomething = (action = "work") => {
-  console.log(`i do something... ${action}`);
+const repeatFunction = (action, interval = 1000, times = 10) => {
+  let counter = 0;
+  const intervalId = setInterval(() => {
+    action();
+    counter++;
+    if (counter === times) clearInterval(intervalId);
+  }, interval);
 };
 
-const timer = setInterval(doSomething, 1000);
+const doSomething = () => {
+  console.log(`i do something... `);
+};
+
+repeatFunction(doSomething);
